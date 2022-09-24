0054bfc3a321a23fbaeed4ea21541f01db9c7426
Pedro <pedrohalmeidamendonca@gmail.com>
2022-09-23 22:41:33 -0300

    Atualizando repositório do dia

--- fundamentos/secao-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/encodeDecode.test.js
+++ fundamentos/secao-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/encodeDecode.test.js
@@ -1,12 +1,8 @@
 const { encode, decode } = require("./encodeDecode");
 
 it("Verifica se encode e decode são funções", () => {
-  expect(() => {
-    encode();
-  }).toThrow();
-  expect(() => {
-    decode();
-  }).toThrow();
+  expect(typeof encode).toBe("function");
+  expect(typeof decode).toBe("function");
 });
 
 it("Verifica se encode converte em numero", () => {
@@ -25,4 +21,18 @@ it("Verifica se decode converte em vogais", () => {
   expect(decode("5")).toMatch(/u/);
 });
 
+it("Verifica se as demais letras/números não são convertidos para cada caso", () => {
+  expect(encode("b")).not.toMatch(/6/);
+  expect(encode("c")).not.toMatch(/7/);
+  expect(encode("d")).not.toMatch(/8/);
+  expect(decode("6")).not.toMatch(/b/);
+  expect(decode("7")).not.toMatch(/c/);
+  expect(decode("8")).not.toMatch(/d/);
+});
 
+it("Verifica se a string que é retornada pelas funções tem o mesmo número de caracteres que a string passada como parâmetro", () => {
+  expect(Object.values(encode("casa")).length).toBe(4);
+  expect(Object.values(encode("porta")).length).toBe(5);
+  expect(Object.values(encode("m2s1")).length).toBe(4);
+  expect(Object.values(encode("c4mp5t1d4r")).length).toBe(10);
+});

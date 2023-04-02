8ff3f018c19b3c49937d3a88b92204c42d6f2b80
Pedro <pedrohalmeidamendonca@gmail.com>
2023-04-02 14:49:44 -0300

    Update files

--- back-end/secao-6-node-js-orm-e-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/src/controllers/Book.js
+++ back-end/secao-6-node-js-orm-e-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/src/controllers/Book.js
@@ -2,40 +2,51 @@ const BookService = require('../services/Book');
 
 const getAll = async (req, res) => {
   const { author } = req.query;
+
   const books = author
     ? await BookService.getByAuthor(author)
     : await BookService.getAll();
+
   return res.status(200).json(books);
 };
 
 const getById = async (req, res) => {
-  const book = await BookService.getById(req.params.id);
+  const { id } = req.params;
+
+  const book = await BookService.getById(id);
+
   if (!book) return res.status(404).json({ message: 'Book not found' });
   return res.status(200).json(book);
 };
 
 const create = async (req, res) => {
   const { title, author, pageQuantity, publisher } = req.body;
+
   const newBook = await BookService.create({ title, author, pageQuantity, publisher });
+
   return res.status(201).json(newBook);
 };
 
 const update = async (req, res) => {
   const { id } = req.params;
   const { title, author, pageQuantity, publisher } = req.body;
+
   const updatedBook = await BookService.update(id, {
     title,
     author,
     pageQuantity,
     publisher,
   });
+
   if (!updatedBook) return res.status(404).json({ message: 'Book not found' });
   return res.status(201).json({ message: 'Book updated' });
 };
 
 const remove = async (req, res) => {
   const { id } = req.params;
+
   const removedBook = await BookService.remove(id);
+
   if (!removedBook) return res.status(404).json({ message: 'Book not found' });
   return res.status(200).json({ message: 'Book removed' });
 };
